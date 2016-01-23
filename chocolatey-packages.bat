f4dc7d4125e0a015e5d260c044ad6127d76cfe4e
Giovanni Tirloni <gpt@gtirloni.com>
2016-01-23 17:53:43 -0200

    Update chocolatey-packages.bat
    Add Java as a dependency

--- chocolatey-packages.bat
+++ chocolatey-packages.bat
@@ -4,3 +4,4 @@ choco install firefox -version 43.0.4 -y
 choco install googlechrome -version 48.0.2564.82 -y
 choco install nodejs -version 4.2.5 -y
 choco install nodejs.install -version 4.2.5 -y
+choco install jre8 -y
