77efe5e06dfd386b67fe6a679009cb720a433bf7
Giovanni Tirloni <gpt@gtirloni.com>
2016-01-23 16:27:26 -0200

    Create chocolatey-packages.bat

---
+++ chocolatey-packages.bat
@@ -0,0 +1,6 @@
+choco install sysinternals -y
+choco install git.install -params '"/GitOnlyOnPath"' -y
+choco install firefox -version 43.0.4 -y
+choco install googlechrome -version 48.0.2564.82 -y
+choco install nodejs -version 4.2.5 -y
+choco install nodejs.install -version 4.2.5 -y
